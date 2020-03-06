d4a45de4d18523d2e8f3fed0b220eaba954793ea
Ahmed Omar <neo21181@gmail.com>
2020-03-06 17:57:10 +0100

    preview project for Button scene changer

---
+++ Assets/Scripts/SceneControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+    public void SceneChanger()
+    {
+        //Debug.Log("did you Click Me!\nNeed Something?");
+        SceneManager.LoadScene("Logo");
+    }
+}
